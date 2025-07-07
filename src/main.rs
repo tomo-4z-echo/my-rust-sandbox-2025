b2b57f4b5049e1d3ae81158a627e2c3449e0d2a0
Tomohiro Takasaki <tomohiro.takasaki@4zws.com>
2025-07-07 17:46:49 +0900

    update guess game.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // message
+    println!("Guess the number!");
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("The secret number is : {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("Your guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
